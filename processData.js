718df25c72128aae1392e3025fd320734ff0ef89
Gregory Melsby <greg27426@gmail.com>
2022-07-26 20:33:18 -0500

    Changed price_change_24h to price_change_percentage_24h

--- processData.js
+++ processData.js
@@ -1,7 +1,7 @@
 export default function processData(data) {
-  const coinList = data.map(({image, name, symbol, current_price, price_change_24h, market_cap}) => 
+  const coinList = data.map(({image, name, symbol, current_price, price_change_percentage_24h, market_cap}) => 
     (
-      { image, name, symbol, current_price, price_change_24h, market_cap }
+      { image, name, symbol, current_price, price_change_percentage_24h, market_cap }
     ));
   
   return coinList;
